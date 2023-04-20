f1ed542570f9a0e9e80010c3fe5a38cafaffaac7
Lex-Stilinski <t50_ya.v.komarkova@mpt.ru>
2023-04-20 22:44:23 +0300

    Практическая №5

---
+++ calendar/AlcoPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    /// <summary>
+    /// Логика взаимодействия для AlcoPage.xaml
+    /// </summary>
+    public partial class AlcoPage : Page
+    {
+        public AlcoPage()
+        {
+            InitializeComponent();
+
+            Alco beer = new Alco();
+            beer.ImageAlco.Source = new BitmapImage(new Uri("/image/beer.png", UriKind.Relative));
+            beer.NameAlco.Text = "Пиво";
+            Alco wine = new Alco();
+            wine.ImageAlco.Source = new BitmapImage(new Uri("/image/wine.png", UriKind.Relative));
+            beer.NameAlco.Text = "Вино";
+            Alco cocktail = new Alco();
+            wine.ImageAlco.Source = new BitmapImage(new Uri("/image/cocktail.png", UriKind.Relative));
+            beer.NameAlco.Text = "Коктейль";
+            Alco whiskey = new Alco();
+            wine.ImageAlco.Source = new BitmapImage(new Uri("/image/whiskey.png", UriKind.Relative));
+            beer.NameAlco.Text = "Виски";
+
+            List<Alco> alcos = new List<Alco>();
+            AlcoList.ItemsSource = alcos;
+        }
+    }
+}
---
+++ calendar/CalendarPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    /// <summary>
+    /// Логика взаимодействия для CalendarPage.xaml
+    /// </summary>
+    public partial class CalendarPage : Page
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+            Calendar.SelectedDate = DateTime.Now;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(+1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(-1);
+        }
+    }
+}
